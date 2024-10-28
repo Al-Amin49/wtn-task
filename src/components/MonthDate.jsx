d2dd14e4db244fcf93b842b2f1f818f1b951179d
Al-Amin49 <alamin496832@gmail.com>
2024-10-28 11:24:57 +0600

    feat:MonthDate component added

--- src/components/MonthDate.jsx
+++ src/components/MonthDate.jsx
@@ -1,17 +1,18 @@
 
-
+import dateIcon from "../assets/Icon.png";
 const MonthDate = () => {
     return (
-        <div className="flex flex-col lg:flex-row items-center mt-8">
-            <div className="w-[332px] border flex">
-            <p>12 Months</p>
-            <p>30 days</p>
-            <p>7 days</p>
-            <p>24 hours</p>
+        <div className="flex flex-col md:flex-row justify-between items-center mt-8">
+            <div className="w-[332px] border flex justify-between items-center px-4 py-1 rounded-md shadow-sm">
+            <p className="border-r pr-4">12 Months</p>
+            <p className="border-r pr-4">30 days</p>
+            <p className="border-r pr-4">7 days</p>
+            <p className="">24 hours</p>
             </div>
 
-            <div>
-                <button>Custome Date</button>
+            <div className=" flex justify-between items-center mt-4 lg:mt-0 border rounded-md px-3 py-1 shadow-sm">
+                <img src={dateIcon} alt="" className="h-5 w-5 mr-2"/>
+                <button className="">Custom Date</button>
             </div>
         </div>
     );
