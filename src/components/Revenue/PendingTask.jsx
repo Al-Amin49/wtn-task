55716507a9db13d98ceb207d26c3878d67b33ac5
Al-Amin49 <alamin496832@gmail.com>
2024-10-28 13:01:31 +0600

    chart.js installed

---
+++ src/components/Revenue/PendingTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PendingTask = () => {
+    return (
+        <div>
+            <h3> This is pending Task</h3>
+        </div>
+    );
+};
+
+export default PendingTask;
