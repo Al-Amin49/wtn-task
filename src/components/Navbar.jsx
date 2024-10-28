a89fcca2e70fdaeb8eb73af81e61677fe6822a1e
Al-Amin49 <alamin496832@gmail.com>
2024-10-28 19:29:47 +0600

    fix:Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <CiSearch className="absolute left-2 mr-2"/>
             <input type="text" placeholder="Search here.."  className="p-2  pl-8" />
             </div>
-            <div className="flex justify-around items-center space-x-10 ">
+            <div className="flex justify-around items-center space-x-3 lg:space-x-10 ">
                 <img src={icons} alt="" className="h-8"/>
                 <div className="border-r-2 h-8 text-[#C2C2C2]"></div>
               
@@ -18,7 +18,7 @@ const Navbar = () => {
                     <h6 className="font-semibold">Nayamul Roni</h6>
                     <span><small className="text-gray-500 text-center">Super Admin</small></span>
                 </div>
-                <img src={profile} alt="" />
+                <img src={profile} alt="" className="hidden md:block"/>
               </div>
             </div>
 
